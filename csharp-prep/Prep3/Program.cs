cda2c2006bea56be77d3d56065b0788a086fa329
IsaiahSamW <Keefe.24601@gmail.com>
2024-09-20 15:37:46 -0400

    IT WORKS! All day, all bugs. But it works!

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,32 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
+
         // asking for the number, turn it into an integer
-        Console.Write("What is the magic number: ");
-        string magicNumber = Console.ReadLine();
-        int parsedMagicNumber = int.Parse(magicNumber);
+        // Console.Write("What is the magic number: ");
+        // string magicNumber = Console.ReadLine();
+        // int parsedMagicNumber = int.Parse(magicNumber);
 
-        // Console.WriteLine(parsedMagicNumber);
+        // Random number generator
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
 
-        // asking for the guess, turning it into a integer
-        Console.Write("What is your guess: ");
-        string guess = Console.ReadLine();
-        int parsedGuess = int.Parse(guess);
+        // Console.WriteLine(magicNumber);
 
-        // Console.WriteLine(parsedGuess);
-
-        //if statments for higher or lower
-        if (parsedGuess > parsedMagicNumber)
+        // hopefully this works...
+       
+        int parsedGuess= 0;
+    
+        while (parsedGuess != magicNumber)
         {
-            Console.WriteLine("Lower");
-        }
-        else if (parsedGuess < parsedMagicNumber)
-        {
-            Console.WriteLine("Higher");
-        }
-        else
-        {
-            Console.WriteLine($"You got it! {parsedMagicNumber} is the Magic Number!");
+            // asking for the guess, turning it into a integer
+            Console.Write("What is your guess: ");
+            string guess = Console.ReadLine();
+            parsedGuess= int.Parse(guess);
+            
+
+            //if statments for lower
+            if (parsedGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            // if statment for higher
+            else if (parsedGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            // only other thing can be correct.
+            else
+            {
+                Console.WriteLine($"You got it! {magicNumber} is the Magic Number!");
+            }
         }
     }
 }
