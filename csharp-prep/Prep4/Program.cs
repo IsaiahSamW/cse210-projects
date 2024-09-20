37e81df7a46362f28a0bb1ed27a4ee3ff08dbcbd
IsaiahSamW <Keefe.24601@gmail.com>
2024-09-20 16:21:41 -0400

    Finishing the bottom bit

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,11 +5,12 @@ class Program
 {
     static void Main(string[] args)
     {
+        // starting with the title, made the list
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
         List<int> numberLis = new List<int>();
 
+        // while loop for adding numbers to list
         int parseNumbers =-1;
-
         while (parseNumbers !=0 )
         {
             Console.Write("Enter number: ");
@@ -21,12 +22,34 @@ class Program
                 numberLis.Add(parseNumbers);
             }
         }
+
+        // foreach loop to math the list
         int numberAdded =0;
+
+        int numberOfThings= 0;
+        
+        int maximumNumber = -10;
         foreach (int number in numberLis)
         {
+            // adding all the numbers together
             // Console.Write($"{number}, ");
             numberAdded = numberAdded +number;
+
+            // counting the number of times looped
+            numberOfThings= numberOfThings +1;
+
+            // if there's a number larget than the maximun, that number becomes the maximun
+            if (number > maximumNumber)
+            {
+                maximumNumber = number;
+            }
         }
-        Console.WriteLine(numberAdded);
+        // typing out the sum, the average, and the largest number
+        Console.WriteLine($"The sum is: {numberAdded}");
+        // Console.WriteLine(numberOfThings);
+        float average = numberAdded/numberOfThings;
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {maximumNumber}");
+
     }
 }
