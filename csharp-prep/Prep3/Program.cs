e614c5e355546f1418866455c158608e0c411704
IsaiahSamW <Keefe.24601@gmail.com>
2024-09-20 09:24:45 -0400

    Starting Prep 3, asked for number, and guess

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // asking for the number, turn it into an integer
+        Console.Write("What is the magic number: ");
+        string magicNumber = Console.ReadLine();
+        int parsedMagicNumber = int.Parse(magicNumber);
+
+        // asking for the guess, turning it into a integer
+        Console.Write("What is your guess: ");
+        string guess = Console.ReadLine();
+        int parsedGuess = int.Parse(guess);
+
+        Console.WriteLine(parsedGuess);
     }
 }
