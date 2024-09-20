3c3e9f5e9fe17cfa8c408b5db686ff8c3cbd8326
IsaiahSamW <Keefe.24601@gmail.com>
2024-09-20 09:32:42 -0400

    wrote the if statements for higher, lower, or correct

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -9,11 +9,27 @@ class Program
         string magicNumber = Console.ReadLine();
         int parsedMagicNumber = int.Parse(magicNumber);
 
+        // Console.WriteLine(parsedMagicNumber);
+
         // asking for the guess, turning it into a integer
         Console.Write("What is your guess: ");
         string guess = Console.ReadLine();
         int parsedGuess = int.Parse(guess);
 
-        Console.WriteLine(parsedGuess);
+        // Console.WriteLine(parsedGuess);
+
+        //if statments for higher or lower
+        if (parsedGuess > parsedMagicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (parsedGuess < parsedMagicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else
+        {
+            Console.WriteLine($"You got it! {parsedMagicNumber} is the Magic Number!");
+        }
     }
 }
