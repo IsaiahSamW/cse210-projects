7340b39d9f68989eda9e5d2b909a024158910db7
IsaiahSamW <Keefe.24601@gmail.com>
2024-09-23 11:07:21 -0400

    working on the next program. Wrote the Job class and working on it working

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,13 @@
+public class Job
+{
+    // Keeps track of the company, job title, start year, and end year.
+    public string _companyName;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} {_companyName} {_startYear}-{_endYear}");
+    }
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //first time writing codes for Class
+        // Adding new job, writing it down
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._companyName = "Microsoft";
+        job1._startYear = 2017;
+        job1._endYear = 2019;
+        
+
+        Job job2 = new Job();
+        job2._jobTitle = "Desk Assistant";
+        job2._companyName = "Apple";
+        job2._startYear = 2020;
+        job2._endYear= 2024;
     }
 }
